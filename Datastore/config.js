43ac24de0aa26825a697f3beedc4c583095a2fba
ARadhitama <ariqradhitamaa@gmail.com>
2019-10-25 13:10:26 +0700

    add route + db connect

--- Datastore/config.js
+++ Datastore/config.js
@@ -1,2 +1,12 @@
 /* Connect Database */
 
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user : 'me',
+    host : 'localhost',
+    database : 'APIPegawai',
+    password : '',
+    port : '8080'
+});
+
+module.exports = pool;
--- Route/index.js
+++ Route/index.js
@@ -1,1 +1,24 @@
 /* route API */
+
+const express = require('express')
+const bodyParser = require ('body-parser')
+const app = express()
+const port = 3000
+const db = require('../Datastore/config')
+
+app.get('/', (request,response) => {
+    response.json({
+        info : 'API performa karyawan ITB'
+        // dokumentasi API
+    }) 
+})
+
+app.get('/assignment/:id', db.getAssignment)
+app.put('/assignment/:id', db.updateAssignment)
+app.get('/user', db.getUser)
+app.get('/user/:id', db.getUserbyID)
+app.get('/performance/:id', db.getPerformance)
+
+app.listen(port, () => {
+    console.log('Running on port ${port}.')
+})
